2e040f00eed353944026ca070330ecc82e82484b
Zahid275 <zahidrasheed287@gmail.com>
2025-03-20 00:17:57 +0500

    fixed a minor issue

--- lib/controllers/detail_screen_controller.dart
+++ lib/controllers/detail_screen_controller.dart
@@ -1,13 +1,12 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class DetailController extends GetxController {
   RxList<Map> castInfo = <Map<String, dynamic>>[].obs;
   String baseUrl = "https://api.themoviedb.org/3/";
-  String apiKey =
-      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2JmNTg3MTEzNTY2MDkzODUxNjA2NTIyN2ZlNTc2NSIsIm5iZiI6MTc0MTkxNTE3MS42ODUsInN1YiI6IjY3ZDM4NDIzZGNiYTcwYTI2OTY0ZTJlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OOxd2-VtbEITdorqnqkmT_o7HPx2hcQe0Gb4xtlFvNs";
-
+  String? apiKey = dotenv.env['API_KEY'];
   getCast(id) async {
     final uri = Uri.parse(baseUrl+ "movie/$id/credits");
 
